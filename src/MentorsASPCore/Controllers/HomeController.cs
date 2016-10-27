dad59f2392398e05a59da80c132a072cdbe46b1c
MrOgorodnikov <v.ogo@yandex.ru>
2016-10-27 19:42:49 +0300

    Show Mentors information

--- src/MentorsASPCore/Controllers/HomeController.cs
+++ src/MentorsASPCore/Controllers/HomeController.cs
@@ -20,16 +20,50 @@ namespace MentorsASPCore.Controllers
         }
         public IActionResult Index()
         {
+            return View();
+        }
+
+        public IActionResult MentorStudent()
+        {
             return View(CreateMentorStudentList());
+        }
+
+        public IActionResult Mentors()
+        {
+            return View(CreateMentorTecnologyList());
+        }
+
+        private List<MentorTecnologyDTO> CreateMentorTecnologyList()
+        {
+            var mtDTO = new List<MentorTecnologyDTO>();
+            var mentorList = db.Mentors
+                            .Include(mt => mt.MentorTecnology)
+                            .ThenInclude(t => t.Tecnology)
+                            .ToList();
+            foreach (var mentor in mentorList)
+            {
+                var tecnologyList = mentor.MentorTecnology
+                                    .Select(t => t.Tecnology)
+                                    .ToList();
+                foreach (var tecnology in tecnologyList)
+                    mtDTO.Add(new MentorTecnologyDTO { Mentor = mentor, Tecnology = tecnology});
+            }
+
+            return mtDTO;
         }
 
         private List<MentorStudentDTO> CreateMentorStudentList()
         {
             var msDTO = new List<MentorStudentDTO>();
-            var mentorList = db.Mentors.Include(ms => ms.MentorStudent).ThenInclude(s => s.Student).ToList();
+            var mentorList = db.Mentors
+                            .Include(ms => ms.MentorStudent)
+                            .ThenInclude(s => s.Student)
+                            .ToList();
             foreach (var mentor in mentorList)
             {
-                var studentList = mentor.MentorStudent.Select(s => s.Student).ToList();
+                var studentList = mentor.MentorStudent
+                                 .Select(s => s.Student)
+                                 .ToList();
                 foreach (var student in studentList)
                     msDTO.Add(new MentorStudentDTO { Mentor = mentor, Student = student });
             }
